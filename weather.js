b00740ccc5f374284433c4ae0d9b414025eea122
Hamza khan <54089367+hamza210@users.noreply.github.com>
2022-01-22 17:46:30 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ class Weather {
     }
 
     async getWeather(){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apikey}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apikey}`);
 
         const responseData = await response.json();
 
